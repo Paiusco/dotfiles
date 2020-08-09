4fb90fbdbf2c7c151e53ef2edd1d0335a8b6bf36
ABP <abpaiusco@stoneridge.com>
2020-08-09 20:28:11 -0300

    Improvements on fish side

---
+++ paiusco.fish
@@ -0,0 +1,15 @@
+if status --is-interactive
+   abbr -ag -- ga. 'git add .'
+   abbr -ag -- gc 'git commit -v'
+   abbr -ag -- gca 'git commit -av'
+   abbr -ag -- gco 'git checkout'
+   abbr -ag -- gitammend 'git commit --ammend'
+   abbr -ag -- gitka 'gitk --all &'
+   abbr -ag -- gpl 'git pull'
+   abbr -ag -- gps 'git push'
+   abbr -ag -- gs 'git status'
+   abbr -ag -- grep 'grep --color=auto'
+   abbr -ag -- l ls
+   abbr -ag -- rmall 'rm -rf *'
+   abbr -ag -- make 'make -j(nproc)'
+end
