f11a73c30e15235057e9dc7c10545736a1d071e9
abp <2374887+Paiusco@users.noreply.github.com>
2022-01-02 00:01:27 +0100

    Add qtile config

---
+++ config/qtile/scripts/autostart.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Set your native resolution IF it does not exist in xrandr
+#More info in the script
+#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-2 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  cp $HOME/.config/qtile/config-azerty.py $HOME/.config/qtile/config.py
+fi
+
+#autostart ArcoLinux Welcome App
+run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
+
+#Some ways to set your wallpaper besides variety or nitrogen
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#start the conky to learn the shortcuts
+(conky -c $HOME/.config/qtile/scripts/system-overview) &
+
+#start sxhkd to replace Qtile native key-bindings
+run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
+
+
+#starting utility applications at boot time
+run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+#starting user applications at boot time
+run volumeicon &
+#run caffeine -a &
+#run thunar &
+#run insync start &
