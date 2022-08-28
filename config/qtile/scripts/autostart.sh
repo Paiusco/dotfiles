5d0d37fe2939388679c6e965c01b8a6501b63588
abp <2374887+Paiusco@users.noreply.github.com>
2022-08-28 11:31:08 +0200

    Cleaning qtile config

--- config/qtile/scripts/autostart.sh
+++ config/qtile/scripts/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function run {
-  if ! pgrep "$1" ;
+  if ! pgrep $1 ;
   then
-    "$@"&
+    $@&
   fi
 }
 
@@ -14,13 +14,14 @@ function run {
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-2 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
+#change your keyboard if you need it
+#setxkbmap -layout be
+
 #autostart ArcoLinux Welcome App
 run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#start the conky to learn the shortcuts
-(conky -c $HOME/.config/qtile/scripts/system-overview) &
 
 #start sxhkd to replace Qtile native key-bindings
 run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
@@ -30,8 +31,6 @@ run nm-applet &
 numlockx on &
 blueberry-tray &
 picom --config $HOME/.config/qtile/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 #starting user applications at boot time
 run volumeicon &
-#run insync start &
