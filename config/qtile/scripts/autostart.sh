dfa07f111d1f32754e6460b6c4231509dcf3b91d
abp <2374887+Paiusco@users.noreply.github.com>
2023-02-17 15:46:04 +0100

    update autostart script

--- config/qtile/scripts/autostart.sh
+++ config/qtile/scripts/autostart.sh
@@ -12,10 +12,16 @@ function run {
 #run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-2 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-2 --off
+
+#change your keyboard if you need it
+setxkbmap -layout us -variant intl
+
+#autostart ArcoLinux Welcome App
+run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 
 #Some ways to set your wallpaper besides variety or nitrogen
-feh --bg-fill /usr/share/backgrounds/arcolinux/wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 
 #start sxhkd to replace Qtile native key-bindings
 run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
@@ -24,7 +30,7 @@ run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 run nm-applet &
 numlockx on &
 blueberry-tray &
-#picom --config $HOME/.config/qtile/scripts/picom.conf &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
 
 #starting user applications at boot time
 run volumeicon &
