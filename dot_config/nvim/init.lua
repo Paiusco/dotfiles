ddc0f546d0644a590a1a6a49ba3cd782f8f8df72
Andre Paiusco <github@paiusco.org>
2025-08-16 22:22:58 +0200

    lsp cleanup done

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
   require("custom.plugins.colourscheme"),
   require("custom.plugins.mini"),
   require("custom.plugins.lsp"),
+  require("custom.plugins.autocompletion"),
   {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
@@ -51,63 +52,18 @@ require("lazy").setup({
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
   -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-
-  -- { -- Autoformat
-  --  "stevearc/conform.nvim",
-  --  event = { "BufWritePre" },
-  --  cmd = { "ConformInfo" },
-  --  keys = {
-  --    {
-  --      "<leader>f",
-  --      function()
-  --        require("conform").format({ async = true, lsp_format = "fallback" })
-  --      end,
-  --      mode = "",
-  --      desc = "[F]ormat buffer",
-  --    },
-  --  },
-  --  opts = {
-  --    notify_on_error = false,
-  --    format_on_save = function(bufnr)
-  --      -- Disable "format_on_save lsp_fallback" for languages that don't
-  --      -- have a well standardized coding style. You can add additional
-  --      -- languages here or re-enable it for the disabled ones.
-  --      local disable_filetypes = { c = true, cpp = true }
-  --      local lsp_format_opt
-  --      if disable_filetypes[vim.bo[bufnr].filetype] then
-  --        lsp_format_opt = "never"
-  --      else
-  --        lsp_format_opt = "fallback"
-  --      end
-  --      return {
-  --        timeout_ms = 500,
-  --        lsp_format = lsp_format_opt,
-  --      }
-  --    end,
-  --    formatters_by_ft = {
-  --      lua = { "stylua" },
-  --      -- Conform can also run multiple formatters sequentially
-  --      python = { "isort", "black" },
-  --      --
-  --      -- You can use 'stop_after_first' to run the first available formatter from the list
-  --      -- javascript = { "prettierd", "prettier", stop_after_first = true },
-  --    },
-  --  },
+  -- {
+  --   -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+  --   -- used for completion, annotations and signatures of Neovim apis
+  --   "folke/lazydev.nvim",
+  --   ft = "lua",
+  --   opts = {
+  --     library = {
+  --       -- Load luvit types when the `vim.uv` word is found
+  --       { path = "luvit-meta/library", words = { "vim%.uv" } },
+  --     },
+  --   },
   -- },
-
-
 
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.lint',
--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -4,44 +4,44 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.opt.winborder = "rounded" -- Make the borders rounded
-vim.opt.swapfile = false -- Remove the swapfile:w
-vim.opt.number = true -- Make line numbers default
-vim.opt.relativenumber = true -- Make relative line numbers default
-vim.opt.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
-vim.opt.autoindent = true -- Copy indent from current line when starting new one
+vim.o.winborder = "rounded" -- Make the borders rounded
+vim.o.swapfile = false -- Remove the swapfile:w
+vim.o.number = true -- Make line numbers default
+vim.o.relativenumber = true -- Make relative line numbers default
+vim.o.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.showmode = false -- Don't show the mode, since it's already in the status line
+vim.o.autoindent = true -- Copy indent from current line when starting new one
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"
+  vim.o.clipboard = "unnamedplus"
 end)
 
-vim.opt.breakindent = true -- Enable break indent
-vim.opt.undofile = true -- Save undo history
+vim.o.breakindent = true -- Enable break indent
+vim.o.undofile = true -- Save undo history
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
-vim.opt.updatetime = 250 -- Decrease update time
-vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time
+vim.o.signcolumn = "yes" -- Keep signcolumn on by default
+vim.o.updatetime = 250 -- Decrease update time
+vim.o.timeoutlen = 300 -- Decrease mapped sequence wait time
 
 -- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.list = true
+vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
-vim.opt.cursorline = true -- Show which line your cursor is on
-vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.o.inccommand = "split" -- Preview substitutions live, as you type!
+vim.o.cursorline = true -- Show which line your cursor is on
+vim.o.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
 
 -- Extra configs gotten from hendrikmi
 vim.o.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
 vim.o.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
-vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+vim.o.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
 vim.o.numberwidth = 2 -- Set number column width to 2 (default: 4)
 vim.o.backspace = "indent,eol,start" -- Allow backspace on (default: 'indent,eol,start')
 vim.o.pumheight = 10 -- Pop up menu height (default: 0)
@@ -51,6 +51,7 @@ vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying mess
 vim.o.backup = false -- Creates a backup file (default: false)
 vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
 vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.o.confirm = true -- if perfoming an :q withtout saving a file, ask if you want to save
 vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
 vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
